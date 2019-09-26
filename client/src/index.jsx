2f55bb983a7c2d708e0df18eb95be92f85402c11
IMan777 <53496709+IMan777@users.noreply.github.com>
2019-09-26 16:52:18 -0400

    File Updated

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { MainView } from './components/main-view/main-view';
+
 import './index.scss'; //Imports SCSS File
 
 class MyFlixApplication extends React.Component{ //Will Be Displayed In HTML Page
     render(){
-        return (
-        <div className="my-flix">
-            <div>Good Morning!</div>
-        </div>
-        );
+        return <MainView/>;
+      
     }
  
 
