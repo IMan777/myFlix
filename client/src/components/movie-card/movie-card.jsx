66aba03b833ae08d80f2101d909f81b6b6e07e62
IMan777 <53496709+IMan777@users.noreply.github.com>
2019-10-02 18:16:25 -0400

    File Updated

--- client/src/components/movie-card/movie-card.jsx
+++ client/src/components/movie-card/movie-card.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './movie-card.scss';
+import Container from 'react-bootstrap/Container';
+export class MovieCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
 
-export class MovieCard extends React.Component{
-    render(){
-        const { movie , onClick } = this.props;
-        
-        return(
-            <div onClick={() => onClick(movie)} className="movie-card">{movie.Title}</div> /* Displays Movie Attributes When Clicked*/ 
-        );  
-    }
+  render() {
+    const {
+      movie,
+      onClick
+    } = this.props;
+    return React.createElement(Container, null, React.createElement(Card, {
+      style: {
+        width: '14rem'
+      },
+      className: "movie-card"
+    }, React.createElement(Card.Img, {
+      variant: "top",
+      src: movie.ImagePath
+    }), React.createElement(Card.Body, null, React.createElement(Card.Title, null, movie.Title), React.createElement(Card.Text, null, movie.Description), React.createElement(Button, {
+      onClick: () => onClick(movie),
+      variant: "dark"
+    }, "Open"))));
+  }
 
 }
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImageURL: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    })
+  }).isRequired,
+  onClick: PropTypes.func.isRequired
+};
