df7bc4b134b98230a7d1e32f67d88f104ed16e12
IMan777 <53496709+IMan777@users.noreply.github.com>
2019-10-02 18:18:16 -0400

    File Updated

--- client/src/components/movie-view/movie-view.jsx
+++ client/src/components/movie-view/movie-view.jsx
@@ -1,53 +1,58 @@
 import React from 'react';
-import { MainView } from '../main-view/main-view';
-
-
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import './movie-view.scss';
+/*import { RegistrationView } from '../registration-view/registration-view';*/
 export class MovieView extends React.Component {
-
   constructor() {
     super();
-    
     this.state = {};
   }
-  navigate(){
-    window.open(<MainView/> ) ; /*Function Created In Order To Return To Main View*/ 
-    
-  }
-  
-  render() {
-    const { movie  } = this.props;
+  /* navigate(){
+     window.open(<RegistrationView/> ) ; 
+     
+   } */
 
+
+  render() {
+    const {
+      movie,
+      onClick
+    } = this.props;
     if (!movie) return null;
     /*Displays Selected Movie Attributes */
-    return (
-       <div className="movie-view">
-        <div className="movie-title">
-          <div className="label">Title</div>
-          <div className="value">{movie.Title}</div>
-        </div>
-        <div className="movie-description">
-          <div className="label">Description</div>
-          <div className="value">{movie.Description}</div>
-        </div>
-         <img className="movie-poster" src={movie.ImagePath} />  
-        <div className="movie-genre">
-          <div className="label">Genre</div>
-          <div className="value">{movie.Genre.Name}</div>
-        </div>
-        <div className="movie-director">
-          <div className="label">Director</div>
-          <div className="value">{movie.Director.Name}</div>
-        </div>
-        <br></br>
-        <button onClick={this.navigate }>Movie List</button> 
-       </div>
-       
 
-    );
-    
+    return React.createElement("div", {
+      className: "movie-view"
+    }, React.createElement(Card.Body, {
+      style: {
+        width: '18rem'
+      },
+      className: "view-movie"
+    }, React.createElement(Card.Img, {
+      variant: "top",
+      className: "movie-poster",
+      src: movie.ImagePath
+    }), React.createElement(Card.Title, {
+      className: "label"
+    }, "Title"), React.createElement(Card.Text, {
+      className: "value"
+    }, movie.Title), React.createElement(Card.Title, {
+      className: "label"
+    }, "Description"), React.createElement(Card.Text, {
+      className: "value"
+    }, movie.Description), React.createElement(Card.Title, {
+      className: "label"
+    }, "Genre"), React.createElement(Card.Text, {
+      className: "value"
+    }, movie.Genre.Name), React.createElement(Card.Title, {
+      className: "label"
+    }, "Director"), React.createElement(Card.Text, {
+      className: "value"
+    }, movie.Director.Name), React.createElement("br", null), React.createElement(Button, {
+      variant: "success",
+      onClick: () => onClick()
+    }, "Movie List")));
   }
- 
+
 }
-
-
-
