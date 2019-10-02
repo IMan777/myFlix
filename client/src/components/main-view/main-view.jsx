e57b4012bce62b1ca10cdf981486e5b8e4d55d42
IMan777 <53496709+IMan777@users.noreply.github.com>
2019-10-02 18:14:48 -0400

    File Updated

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,59 +1,104 @@
 import React from 'react';
 import axios from 'axios';
 import { MovieCard } from '../movie-card/movie-card';
-import { MovieView} from '../movie-view/movie-view';
-
-
+import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
+import { RegistrationView } from '../registration-view/registration-view';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 export class MainView extends React.Component {
-
-  constructor() {
-    
-    super();
-
-    
+  constructor(props) {
+    super(props);
     this.state = {
       movies: null,
-      selectedMovie: null
+      selectedMovie: null,
+      user: null,
+      newUser: false
     };
   }
 
- 
   componentDidMount() {
-    axios.get('https://my-flix-10.herokuapp.com/movies') /*Endpoint To Access Movie List From DataBase/API  */
-      .then(response => {
-        
-        this.setState({
-          movies: response.data
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
+    axios.get('https://my-flix-10.herokuapp.com/movies')
+    /*Endpoint To Access Movie List From DataBase/API  */
+    .then(response => {
+      this.setState({
+        movies: response.data
       });
+    }).catch(function (error) {
+      console.log(error);
+    });
   }
 
-  onMovieClick(movie){
+  onMovieClick(movie) {
     this.setState({
       selectedMovie: movie
     });
   }
 
+  clickButton() {
+    this.setState({
+      selectedMovie: null
+    });
+  }
+
+  onLoggedIn(user) {
+    this.setState({
+      user
+    });
+  }
+
+  newRegistration() {
+    /*Function To Register A New Member*/
+    this.setState({
+      newUser: true
+    });
+  }
+
+  newMember() {
+    this.setState({
+      /*New Member Established*/
+      newUser: null
+    });
+  }
 
   render() {
-    
-    const { movies, selectedMovie } = this.state;
+    const {
+      movies,
+      selectedMovie,
+      user,
+      newUser
+    } = this.state;
 
-    
-    if (!movies) return <div className="main-view"/>;
+    if (!user) {
+      if (newUser) return React.createElement(RegistrationView, {
+        newMember: () => this.newMember(),
+        onLoggedIn: user => this.onLoggedIn(user)
+      });else return React.createElement(LoginView, {
+        onLoggedIn: user => this.onLoggedIn(user),
+        newUser: () => this.newRegistration(),
+        newMember: () => this.newMember()
+      });
+    }
 
-    return (
-      <div className="main-view">
-       {selectedMovie
-          ? <MovieView movie={selectedMovie}/>
-          : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
-          ))
-       }
-      </div>
-     );
-   }
+    if (!user) return React.createElement(LoginView, {
+      onLoggedIn: user => this.onLoggedIn(user)
+    });
+    if (!movies) return React.createElement("div", {
+      className: "main-view"
+    });
+    return React.createElement("div", {
+      className: "main-view"
+    }, React.createElement(Container, {
+      className: "mainContain"
+    }, React.createElement(Row, null, selectedMovie ? React.createElement(MovieView, {
+      movie: selectedMovie,
+      onClick: () => this.clickButton()
+    }) : movies.map(movie => React.createElement(Col, null, React.createElement(MovieCard, {
+      key: movie._id,
+      movie: movie,
+      onClick: movie => this.onMovieClick(movie)
+    }))))));
   }
+
+}
