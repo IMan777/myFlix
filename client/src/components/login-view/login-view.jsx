4d4edca572299bce48f2864ad6febf0f8fd25e9a
IMan777 <53496709+IMan777@users.noreply.github.com>
2019-10-06 14:58:13 -0400

    File Updated

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container'
@@ -10,13 +9,13 @@ import './login-view.scss'
 
 export function LoginView(props) {
     
-  const [ username, enterUsername ] = useState('');
-  const [ password, enterPassword ] = useState('');
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
 
  const handleSubmit = (e) => {
     e.preventDefault();
     
-    axios.post('https://my-flix-10.herokuapp.com/login', {
+    axios.post('https://my-flix-10.herokuapp.com/login', { 
       Username: username,
       Password: password
     })
@@ -29,30 +28,19 @@ export function LoginView(props) {
     });
   }; 
 
- /* const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(username, password);
-    props.onLoggedIn(username)
-  };*/
-  
-
-
-  
-
- return (
+ 
+  return (
      <Container>
     <Form className='loginForm'>
       <Form.Group controlId ="formBasicUsername">
       <Form.Label>Username:</Form.Label>
-        <Form.Control type="text" placeholder="Enter Username" value={username} onChange={e => enterUsername(e.target.value)} />
+        <Form.Control type="text" placeholder="Enter Username" value={username} onChange={e => setUsername(e.target.value)} />
       </Form.Group>
       <Form.Group controlId ="formBasicPassword">  
       <Form.Label>Password:</Form.Label>
-        <Form.Control type="password"  placeholder="Enter Password" value={password} onChange={e => enterPassword(e.target.value)} />
+        <Form.Control type="password"  placeholder="Enter Password" value={password} onChange={e => setPassword(e.target.value)} />
       </Form.Group>
-        <Form.Group controlId="formBasicCheckBox">
-        <Form.Check type="checkbox" label="Check Me Out"/>
-       </Form.Group>
+        
       <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
       
     </Form>
@@ -65,9 +53,3 @@ export function LoginView(props) {
 
 
 
-LoginView.propTypes ={
-    
-    onLoggedIn: PropTypes.func.isRequired,
-   
- 
- };
