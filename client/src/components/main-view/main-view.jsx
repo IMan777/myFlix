d57ce5e25cac6f67d8f13d9f882636094e73c7ac
IMan777 <53496709+IMan777@users.noreply.github.com>
2019-10-06 14:48:40 -0400

    File Updated

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import axios from 'axios';
+
+/*import { BrowserRouter as Router, Route} from "react-router-dom";*/
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view';
@@ -19,7 +22,7 @@ export class MainView extends React.Component {
   }
 
   componentDidMount() {
-    axios.get('https://my-flix-10.herokuapp.com/movies')
+    axios.get('https://my-flix-10.herokuapp.com/movies') /*Wont't Be Needed Eventually */
     
     .then(response => {
       this.setState({
@@ -30,25 +33,20 @@ export class MainView extends React.Component {
     });
   }
 
-  onMovieClick(movie) {
+  onMovieClick(movie) {  /*Wont't Be Needed Eventually */
     this.setState({
       selectedMovie: movie
     });
   }
 
-  clickButton() {
+  clickButton() {  /*Wont't Be Needed Eventually */
     this.setState({
       selectedMovie: null
     });
   }
 
- onLoggedIn(user) {
-    this.setState({
-      user
-    });
-  }
-
-  /*onLoggedIn(authData) {
+ 
+  onLoggedIn(authData) {
     console.log(authData);
     this.setState({
       user: authData.user.Username
@@ -58,6 +56,17 @@ export class MainView extends React.Component {
     localStorage.setItem('user', authData.user.Username);
     this.getMovies(authData.token);
   }
+
+  onLoggedOut (){
+     localStorage.removeItem('token');
+     localStorage.removeItem('user');
+     this.setState({
+       user:null
+      })
+     widow.open('/', _self);
+    }
+
+
   getMovies(token) {
     axios.get('https://my-flix-10.herokuapp.com/movies', {
       headers: { Authorization: `Bearer ${token}`}
@@ -71,17 +80,42 @@ export class MainView extends React.Component {
     .catch(function (error) {
       console.log(error);
     });
-  }*/
+  }
+
+  /*getUser(token) {
+    axios.get('https://my-flix-10.herokuapp.com/users', {
+      headers: { Authorization: `Bearer ${token}`}
+    })
+    .then(response => {
+     
+      this.setState({
+       users: response.data
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  updateUser(data){
+  this.setState({
+  userInfo.data
+
+})
+ localStorage.setItem('user', data.Username);
+}
+
+*/ 
 
   newRegistration() {
    
     this.setState({
-      newUser: true
+      newUser: true  /*Wont't Be Needed Eventually */
     });
   }
 
   newMember() {
-    this.setState({
+    this.setState({   /*Wont't Be Needed Eventually */
       
       newUser: null
     });
@@ -92,24 +126,26 @@ export class MainView extends React.Component {
       movies,
       selectedMovie,
       user,
-      newUser
+      
     } = this.state;
 
-    if(!user){
+   /* if(!user){
 
       if(newUser) return <RegistrationView newMember={ () => this.newMember()} onLoggedIn={user => this.onLoggedIn(user)}/>;
       
       else return <LoginView onLoggedIn = {user => this.onLoggedIn(user)} newUser = {() => this.newRegistration()} newMember={() =>this.newMember()}/>;
       
-      }
+      }; */
       
       
       
-      
-        if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;  
+         if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;  
+        
           
           if (!movies) return <div className="main-view"/>;
-      
+          
+          
+
           return (
             <div className="main-view">
              <Container>
@@ -126,6 +162,29 @@ export class MainView extends React.Component {
             
              </Container>
             </div>
+           
+           /*<Router>
+         <div className="main-view">
+         <Route exact path="/" render={() => {if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;return movies.map(m => <MovieCard key={m._id} movie={m}/>)
+  }}/>
+           <Route path="/register" render={() => <RegistrationView />} />
+          <Route exact path="/" render={() => movies.map(m => <MovieCard key={m._id} movie={m}/>)}/>
+          <Route path="/movies/:movieId" render={({match}) => <MovieView movie={movies.find(m => m._id === match.params.movieId)}/>}/>
+         </div>
+         <Route path="/directors/:name" render={({ match }) => {if (!movies) return <div className="main-view"/>; 
+  return <DirectorView director={movies.find(m => m.Director.Name === match.params.name).Director}/>}
+} />
+          <Route path="/genres/:name" render={({ match }) => {if (!movies) return <div className="main-view"/>;
+  return <GenreView director={movies.find(m => m.Genre.Name === match.params.name).Genre}/>}
+} />
+          <Route path="/users/:Username" render={({match}) =>{return <ProfileView userInfo={userInfo} />}} />
+          <Route path="/edit/:Username" render={() => <ProfileEdit userInfo={userInfo} user={user} token={token} updateUser={data => this.updateUser(data)} />} />
+
+
+</Router> */
+           
+           
+           
            );
          }
         }
