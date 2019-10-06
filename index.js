f51fab9f0fcd7e973fe87b4c246cf87af3e8b783
IMan777 <53496709+IMan777@users.noreply.github.com>
2019-10-06 13:43:59 -0400

    File Updated

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ const auth = require('./auth') (app);
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:5000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, DELETE,PUT, OPTIONS ");
   next();
 });
 
