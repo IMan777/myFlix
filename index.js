a11e75f315f690fedcccab3d2c5faf337c7f13ab
IMan777 <53496709+IMan777@users.noreply.github.com>
2019-10-06 14:05:12 -0400

    File Updated

--- index.js
+++ index.js
@@ -31,14 +31,14 @@ const auth = require('./auth') (app);
 
 /*Cors Script To Allow Only Certain URLs Access*/
 
-const corsOptions = {
+/*const corsOptions = {
   origin: [process.env.URL, 'http://localhost:5000']
   }
   app.use(cors(corsOptions))
-  app.options('*', cors(corsOptions),
+  app.options('*', cors(corsOptions),*/
 
 
-/*app.use(cors()); */
+app.use(cors()); 
 
 /*app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:5000");
