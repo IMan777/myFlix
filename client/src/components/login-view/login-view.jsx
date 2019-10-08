71d8f994c6bd6c46391e80d275cd14f06bce8138
IMan777 <53496709+IMan777@users.noreply.github.com>
2019-10-07 20:22:09 -0400

    File Updated

--- client/src/components/login-view/login-view.jsx
+++ client/src/components/login-view/login-view.jsx
@@ -1,55 +1,64 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Container from 'react-bootstrap/Container'
-import './login-view.scss'
-
-
+import React, { useState } from "react";
+import axios from "axios";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import { Link } from "react-router-dom";
+import "./login-view.scss";
 
 export function LoginView(props) {
-    
-  const [ username, setUsername ] = useState('');
-  const [ password, setPassword ] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
- const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
-    
-    axios.post('https://my-flix-10.herokuapp.com/login', { 
-      Username: username,
-      Password: password
-    })
-    .then(response => {
-      const data = response.data;
-      props.onLoggedIn(data);
-    })
-    .catch(e => {
-      console.log('Nonexistent User')
-    });
-  }; 
 
- 
+    axios
+      .post("https://my-flix-10.herokuapp.com/login", {
+        Username: username,
+        Password: password
+      })
+      .then(response => {
+        const data = response.data;
+        props.onLoggedIn(data);
+      })
+      .catch(e => {
+        console.log("Nonexistent User");
+        alert("Nonexistent User. Please Try Again.");
+      });
+  };
+
   return (
-     <Container>
-    <Form className='loginForm'>
-      <Form.Group controlId ="formBasicUsername">
-      <Form.Label>Username:</Form.Label>
-        <Form.Control type="text" placeholder="Enter Username" value={username} onChange={e => setUsername(e.target.value)} />
-      </Form.Group>
-      <Form.Group controlId ="formBasicPassword">  
-      <Form.Label>Password:</Form.Label>
-        <Form.Control type="password"  placeholder="Enter Password" value={password} onChange={e => setPassword(e.target.value)} />
-      </Form.Group>
-        
-      <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
-      
-    </Form>
+    <Container>
+      <Form className="loginForm">
+        <Form.Group controlId="formBasicUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Username"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password:</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </Form.Group>
+        <div>
+          <Button variant="primary" type="submit" onClick={handleSubmit}>
+            Log In
+          </Button>
+          <br></br>
+          <Link to={"/register"}>
+            <Button variant="dark">Resigter</Button>
+          </Link>
+        </div>
+      </Form>
     </Container>
   );
-
-    
 }
-
-
-
-
