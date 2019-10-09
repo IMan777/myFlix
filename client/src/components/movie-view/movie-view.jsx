614f98f0aa72271f85eec0275473e960e0afdd1e
IMan777 <53496709+IMan777@users.noreply.github.com>
2019-10-08 23:45:54 -0400

    File Updated

--- client/src/components/movie-view/movie-view.jsx
+++ client/src/components/movie-view/movie-view.jsx
@@ -1,67 +1,65 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 import Card from 'react-bootstrap/Card';
 import './movie-view.scss';
 
 import { Link } from "react-router-dom";
 
-function addMovie(event) {
-  event.preventDefault();
-  axios.post(`https://my-flix-10.herokuapp.com/users/${localStorage.getItem('user')}/FavouriteFilms/${movie._id}`, {
-    Username: localStorage.getItem('user')
-  }, {
-    headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
-  })
-    .then(response => {
-      console.log(response);
-      alert('This Movie Has Been Added To Your Favorites');
+
+
+
+export function MovieView(props) {
+
+
+
+  const {
+    movie
+  } = props;
+  if (!movie) return null;
+  /*Displays Selected Movie Attributes */
+  function addMovie(event) {
+    event.preventDefault();
+    console.log('click click boom')
+    axios.post(`https://my-flix-10.herokuapp.com/users/${localStorage.getItem('user')}/movies/${movie._id}`, {
+      Username: localStorage.getItem('user')
+    }, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
     })
-    .catch(event => {
-      console.log('Error!');
-      alert('Error! Movie Not Added');
-    });
-};
+      .then(response => {
+        console.log(response);
+        alert('This Movie Has Been Added To Your Favorites');
+      })
+      .catch(event => {
+        console.log('Error!');
+        alert('Error! Movie Not Added');
+      });
+  };
 
+  return (
+    <div >
+      <Card className="view-movie">
+      <Card.Img variant="top" className="movie-poster" src={movie.ImagePath} />
+      <Card.Body style={{ width: '16rem' }} >
+        <Card.Title className="label">Title</Card.Title>
+        <Card.Text className="value">{movie.Title}</Card.Text>
 
-export class MovieView extends React.Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  render() {
-    const {
-      movie,
-      onClick
-    } = this.props;
-    if (!movie) return null;
-    /*Displays Selected Movie Attributes */
-
-    return (
-    <div className="movie-view">
-
-<Card.Img variant="top" className="movie-poster" src={movie.ImagePath} />  
-        <Card.Body style={{ width: '14rem' }}>
-          <Card.Title className="label">Title</Card.Title>
-          <Card.Text className="value">{movie.Title}</Card.Text>
-        
-          <Card.Title className="label">Description</Card.Title>
-          <Card.Text className="value">{movie.Description}</Card.Text>
-          <div>
+        <Card.Title className="label">Description</Card.Title>
+        <Card.Text className="value">{movie.Description}</Card.Text>
+        <div>
           <Link to={`/genres/${movie.Genre.Name}`}><Button variant="link">Genre</Button></Link>
           <Link to={`/directors/${movie.Director.Name}`}><Button variant="link">Director</Button></Link>
-          <Link to={'/'}><Button variant="success" >Movie List</Button> </Link>
-           
-        <Button variant="outline-secondary" onClick={event => addMovie(event)}> Add to Favourites </Button>
-        </div>
-       </Card.Body>
-       
-       
-       <Link to={`/`}><Button variant="outline-info">Return</Button></Link> 
-</div>
-       
+          <Link to={'/'}><Button variant="success" size='sm' >Movies</Button> </Link>
 
-    );
-    
-  }
+               <Button variant="outline-primary" size="lg" onClick={event => addMovie(event)}> Add to Favourites </Button>
+        </div>
+      </Card.Body>
+      </Card>
+
+            
+    </div>
+
+
+  );
+
 }
