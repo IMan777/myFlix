968f61acf2313bf7788148e47dd8ddec61a82bc8
IMan777 <53496709+IMan777@users.noreply.github.com>
2019-10-15 19:22:40 -0400

    File Updated

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
-import { MainView } from './components/main-view/main-view';
+import  MainView  from './components/main-view/main-view';
+import moviesApp from './reducers/reducers';
+
 
 import './index.scss'; //Imports SCSS File
 
+const store = createStore(moviesApp);
+
 class MyFlixApplication extends React.Component{ //Will Be Displayed In HTML Page
     render(){
-        return <MainView/>;
-      }
+        return (
+        <Provider store ={store}>
+          <MainView/>
+       </Provider>  
+       );  
+    }
  }
 
 const container = document.getElementsByClassName('app-container')[0];
 
 ReactDOM.render(React.createElement(MyFlixApplication), container);
+
+
