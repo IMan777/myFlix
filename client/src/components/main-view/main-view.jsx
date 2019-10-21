c42372d56517ee41088c35aa3139edcd04bba6c8
IMan777 <53496709+IMan777@users.noreply.github.com>
2019-10-21 19:57:31 -0400

    File Updated

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -152,7 +152,7 @@ import "./main-view.scss";
             <Route 
               path="/movies/:movieId" 
               render={({match}) => 
-              <MovieView movieId={match.params.movieId}/>}
+              <MovieView movies={match.params.movieId}/>}
               />
             <Route
               path="/directors/:name"
